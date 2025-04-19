bcc0878cf643fe5fb2a568e39e7b7fc1ba7b6de3
Michal Kmet <michalkmet6@gmail.com>
2025-04-19 21:33:52 +0200

    personal details form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { BookingDatePageComponent } from './booking-date-page/booking-date-page.component';
+import { PersonalDetailsPageComponent } from './personal-details-page/personal-details-page.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, BookingDatePageComponent],
+  imports: [CommonModule, BookingDatePageComponent, PersonalDetailsPageComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/personal-details-page/personal-details-page.component.ts
+++ src/app/personal-details-page/personal-details-page.component.ts
@@ -1,10 +1,49 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-personal-details-page',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './personal-details-page.component.html',
   styleUrl: './personal-details-page.component.css',
 })
-export class PersonalDetailsPageComponent {}
+export class PersonalDetailsPageComponent {
+  personalDetailsForm = new FormGroup({
+    firstName: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    lastName: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    personalIndentificationNumber: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    country: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    city: new FormControl('', {
+      validators: [Validators.required],
+    }),
+    email: new FormControl('', {
+      validators: [Validators.required, Validators.email],
+    }),
+  });
+
+  get emailIsInvalid() {
+    return (
+      this.personalDetailsForm.controls.email.invalid &&
+      this.personalDetailsForm.controls.email.touched &&
+      this.personalDetailsForm.controls.email.dirty
+    );
+  }
+
+  onSubmit() {
+    console.log('this.form: ', this.personalDetailsForm.value);
+  }
+}
