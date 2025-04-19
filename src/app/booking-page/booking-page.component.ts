409e36916d2ce3b403828431ec5fef7a89158a6f
Michal Kmet <michalkmet6@gmail.com>
2025-04-19 13:57:01 +0200

    booking-page component

---
+++ src/app/booking-page/booking-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-booking-page',
+  imports: [CommonModule],
+  templateUrl: './booking-page.component.html',
+  styleUrl: './booking-page.component.css',
+})
+export class BookingPageComponent {}
