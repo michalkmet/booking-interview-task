96fa66f63a30122bee638a837da35c34b34e6a78
Michal Kmet <michalkmet6@gmail.com>
2025-04-21 22:50:16 +0200

    routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { BookingDatePageComponent } from './booking-date-page/booking-date-page.component';
-import { PersonalDetailsPageComponent } from './personal-details-page/personal-details-page.component';
-
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, BookingDatePageComponent, PersonalDetailsPageComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Route } from '@angular/router';
+import { BookingDatePageComponent } from './booking-date-page/booking-date-page.component';
+import { PersonalDetailsPageComponent } from './personal-details-page/personal-details-page.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', component: BookingDatePageComponent },
+  { path: 'date', component: BookingDatePageComponent },
+  { path: 'personal', component: PersonalDetailsPageComponent },
+  { path: '**', component: BookingDatePageComponent },
+];
