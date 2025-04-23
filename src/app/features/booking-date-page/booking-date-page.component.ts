28bfc619b67411923e6791214860f4a6634d8d42
Michal Kmet <michalkmet6@gmail.com>
2025-04-23 13:22:32 +0200

    highlighted dates

--- src/app/features/booking-date-page/booking-date-page.component.ts
+++ src/app/features/booking-date-page/booking-date-page.component.ts
@@ -1,14 +1,21 @@
-import { Component, inject, model, OnInit } from '@angular/core';
+import { Component, inject, model, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BookingDateService } from './booking-date-service.service';
 import { AvailableSlots } from './available-slot.model';
 import { HeaderComponent } from '../../shared/header/header.component';
 
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import {provideNativeDateAdapter} from '@angular/material/core';
+import { MatCalendar } from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 import { MatCardModule } from '@angular/material/card';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatListModule} from '@angular/material/list';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-booking-date-page',
@@ -20,7 +27,9 @@ import {MatListModule} from '@angular/material/list';
     MatDatepickerModule,
     ReactiveFormsModule,
     FormsModule,
-    MatListModule
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatCalendar,
   ],
   templateUrl: './booking-date-page.component.html',
   styleUrl: './booking-date-page.component.css',
@@ -28,11 +37,14 @@ import {MatListModule} from '@angular/material/list';
 export class BookingDatePageComponent implements OnInit {
   private bookingDateService = inject(BookingDateService);
   public availableSlots: AvailableSlots = {};
-  selected = model<Date | null>(null);
+  selectedDate = model<Date | null>(null);
+  datesToHighlight: string[] = [];
+  isLoading = true;
+  @ViewChild(MatCalendar) calendar!: MatCalendar<Date>;
 
   bookingForm = new FormGroup({
-    date: new FormControl<Date | null>(null),
-    timeSlots: new FormControl<string | null>(null),
+    date: new FormControl<string | null>(null),
+    timeSlot: new FormControl<string | null>(null),
   });
 
   ngOnInit() {
@@ -43,10 +55,38 @@ export class BookingDatePageComponent implements OnInit {
       console.log('Response:', response);
       this.availableSlots = response.slots;
       console.log('Available Slots:', this.availableSlots);
+
+      for (const [key, value] of Object.entries(this.availableSlots)) {
+        console.log(`Date: ${key}`);
+        console.log(`${key}: ${value}`);
+
+        this.datesToHighlight.push(key);
+      }
+      console.log('datesToHighlight1:', this.datesToHighlight);
+
+      if (this.calendar) {
+        this.calendar.updateTodaysDate();
+      }
+      this.isLoading = false;
     });
   }
 
-  onSubmit(){
+  formatDate(date: Date): string {
+    const day = String(date.getDate()).padStart(2, '0');
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const year = date.getFullYear();
+    return `${day}/${month}/${year}`;
+  }
+
+  dateClass = (date: Date): string => {
+    const key = this.formatDate(date);
+    console.log('key:', key);
+    console.log('this.availableSlots:', this.availableSlots);
+    console.log('this.availableSlots[key]:', this.availableSlots[key]);
+    return this.availableSlots[key] ? 'available-date' : '';
+  };
+
+  onSubmit() {
     console.log('submitted');
     console.log(this.bookingForm.value);
   }
